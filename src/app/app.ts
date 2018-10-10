fe40fc0cdd64812bfe0e2104d07f70a7f201192a
Bao Tran <trangiabao1203@gmail.com>
2018-10-11 00:58:07 +0700

    Update "cors"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import BaseRouting from '../shared/base/base.routing';
+import cors from 'cors';
 
 class App {
     public app: express.Application;
@@ -8,6 +9,13 @@ class App {
         this.app = express();
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(cors({
+            allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token', 'Authorization'],
+            credentials: true,
+            methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+            origin: '*',
+            preflightContinue: false
+        }));
 
         new BaseRouting(this.app);
     }
