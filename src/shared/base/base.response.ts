93921dbddab39edc9ac300240025864192e3e89f
Bao Tran <trangiabao1203@gmail.com>
2018-10-11 00:58:22 +0700

    Fix BaseResponse

--- src/shared/base/base.response.ts
+++ src/shared/base/base.response.ts
@@ -14,40 +14,40 @@ export class BaseResponse {
 
         // Khác
         if (typeof data === 'string' || data instanceof String) {
-            return res.status(200).json({ status: false, code: 200, msg: data }).end();
+            return res.status(200).json({ status: true, code: 200, msg: data }).end();
         }
 
         if (req.method === 'GET') {
             let msg = 'Lấy dữ liệu thành công.';
             if (total > 0) {
-                return res.status(200).json({ status: false, code: 200, msg: msg, data: data, total: total }).end();
+                return res.status(200).json({ status: true, code: 200, msg: msg, data: data, total: total }).end();
             }
-            return res.status(200).json({ status: false, code: 200, msg: msg, data: data }).end();
+            return res.status(200).json({ status: true, code: 200, msg: msg, data: data }).end();
         }
 
         if (req.method === 'POST') {
             let msg = 'Thêm dữ liệu thành công.';
-            return res.status(200).json({ status: false, code: 200, msg: msg, data: data }).end();
+            return res.status(200).json({ status: true, code: 200, msg: msg, data: data }).end();
         }
 
         if (req.method === 'PUT') {
             let msg = 'Cập nhật dữ liệu thành công.';
-            return res.status(200).json({ status: false, code: 200, msg: msg, data: data }).end();
+            return res.status(200).json({ status: true, code: 200, msg: msg, data: data }).end();
         }
 
         if (req.method === 'PATCH') {
             let msg = 'Thay đổi trạng thái dữ liệu thành công.';
-            return res.status(200).json({ status: false, code: 200, msg: msg, data: data }).end();
+            return res.status(200).json({ status: true, code: 200, msg: msg, data: data }).end();
         }
 
         if (req.method === 'DELETE') {
             let msg = 'Xóa dữ liệu thành công.';
-            return res.status(200).json({ status: false, code: 200, msg: msg, data: data }).end();
+            return res.status(200).json({ status: true, code: 200, msg: msg, data: data }).end();
         }
 
         // Default
         let msg = 'Thao tác được xử lý thành công.';
-        return res.status(200).json({ status: false, code: 200, msg: msg, data: data }).end();
+        return res.status(200).json({ status: true, code: 200, msg: msg, data: data }).end();
     }
 
 	/**
@@ -58,34 +58,34 @@ export class BaseResponse {
         // Lỗi 401: Unauthorized
         if (error.name === 'JsonWebTokenError') {
             let msg = 'Xác thực thất bại. Vui lòng đăng nhập lại.';
-            return res.status(401).json({ status: false, code: 401, name: error.name, msg: msg }).end();
+            return res.status(201).json({ status: false, code: 401, name: error.name, msg: msg }).end();
         }
 
         if (error.name === 'TokenExpiredError') {
             let msg = 'Phiên giao dịch hết hạn. Vui lòng đăng nhập lại.';
-            return res.status(401).json({ status: false, code: 401, name: error.name, msg: msg }).end();
+            return res.status(201).json({ status: false, code: 401, name: error.name, msg: msg }).end();
         }
 
         if (error.name === 'AccountNotExist') {
             let msg = 'Tài khoản không tồn tại trong hệ thống.';
-            return res.status(401).json({ status: false, code: 401, name: error.name, msg: msg }).end();
+            return res.status(201).json({ status: false, code: 401, name: error.name, msg: msg }).end();
         }
 
         if (error.name === 'WrongPassword') {
             let msg = 'Mật khẩu không chính xác. Vui lòng kiểm tra lại.';
-            return res.status(401).json({ status: false, code: 401, name: error.name, msg: msg }).end();
+            return res.status(201).json({ status: false, code: 401, name: error.name, msg: msg }).end();
         }
 
         // Lỗi 403: Forbidden
         if (error.name === 'BlockAccount') {
             let msg = 'Tài khoản của bạn đã bị khóa. Vui lòng liên hệ ban quản trị.';
-            return res.status(401).json({ status: false, code: 401, name: error.name, msg: msg }).end();
+            return res.status(201).json({ status: false, code: 401, name: error.name, msg: msg }).end();
         }
 
         // Lỗi 404: Not found
         if (error.name === 'NoContent') {
             let msg = 'Không tìm thấy dữ liệu hợp lệ.';
-            return res.status(404).json({ status: false, code: 404, name: error.name, msg: msg }).end();
+            return res.status(201).json({ status: false, code: 404, name: error.name, msg: msg }).end();
         }
 
         // Lỗi 201: Created
